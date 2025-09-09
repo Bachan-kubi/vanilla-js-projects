5dadc6e438d3de9fccf8d10be57bc13d757b27cc
Bachan-kubi <felixghagra@gmail.com>
2025-09-09 19:00:29 +0600

    to do app add task completed

---
+++ 01_todo_list/main.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById("add-task");
+const newTask = document.getElementById("new-task");
+const listContainer = document.querySelector("ol");
+
+addBtn.addEventListener("click", addTask);
+
+function  addTask() {
+    const taskAdded = newTask.value;
+    const list = document.createElement('li');
+    list.innerHTML = taskAdded;
+    listContainer.appendChild(list);
+
+}
