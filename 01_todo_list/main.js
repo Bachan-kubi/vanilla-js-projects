2e7d7ff896636a15f99ea7a3a2b544044b7400d2
Bachan-kubi <felixghagra@gmail.com>
2025-09-11 08:01:40 +0600

    to do app add task completed

--- 01_todo_list/main.js
+++ 01_todo_list/main.js
@@ -1,12 +1,44 @@
-const addBtn = document.getElementById("add-task");
-const newTask = document.getElementById("new-task");
-const listContainer = document.querySelector("ol");
+const addButton = document.getElementById("addBtn");
+const inputTask = document.getElementById("input-task");
+const taskList = document.getElementById("task-list");
 
-addBtn.addEventListener("click", addTask);
+let allTasks = [];
 
-function  addTask() {
-    const taskAdded = newTask.value;
-    const list = document.createElement('li');
-    list.innerHTML = taskAdded;
-    listContainer.appendChild(list);
+addButton.addEventListener("click", addTask);
+
+function addTask(e) {
+    e.preventDefault();
+    const newTask = inputTask.value.trim();
+    if(newTask){
+        allTasks.push({task:newTask, completed: false})
+    }
+    console.log(allTasks);
+    inputTask.value = "";
+    updateTask();
+
 }
+
+function updateTask() {
+    allTasks.forEach((task, index)=>{
+        console.log(task, index);
+        const list = document.createElement("li");
+        list.innerHTML = "";
+        list.innerHTML = `
+            <div class="task-item">
+                <div class ="task ${task.completed? "completed":""}">
+                    <input type="checkbox" class="check-box" ${task.completed? "checked": ""}/>
+                    <h2>${task.task}</h2>
+                </div>
+                <div class="icons">
+                    <img src="./img/edit.png" onClick= editTask(${index})/>
+                    <img src="./img/bin.png" onClick= deleteTask(${index})/>
+                </div>
+            </div>
+        `
+        list.addEventListener("change", ()=>toggelTaskCompleted(index));
+        taskList.append(list);
+        
+    });
+}
+
+
