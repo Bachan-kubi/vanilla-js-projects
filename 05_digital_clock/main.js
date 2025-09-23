4596d2ac7633d76781bd0552e93e5487bc3b1330
Bachan-kubi <felixghagra@gmail.com>
2025-09-23 11:30:57 +0600

    digital clock built

--- 05_digital_clock/main.js
+++ 05_digital_clock/main.js
@@ -1,5 +1,8 @@
 function  updateClock() {
+    const timeSlot = document.getElementById("time")
     const date = new Date();
     console.log(date.getFullYear());
+    timeSlot.innerHTML = 
+    date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
 }
-updateClock();
+setInterval(updateClock, 1000);
