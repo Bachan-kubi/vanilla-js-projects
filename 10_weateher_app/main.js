dcb821365e1f3da791bc53bf865cba818ff2fce4
Bachan Kubi <felixghagra@gmail.com>
2025-10-08 00:32:33 +0600

    logic are being built

--- 10_weateher_app/main.js
+++ 10_weateher_app/main.js
@@ -1,6 +1,9 @@
 const apiKey = "c14f92927d3fbdbfcf9d1934d5bc5655";
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-button");
+const notFoundSection = document.querySelector(".not-found");
+const searchCitySection = document.querySelector(".search-city");
+const weatherInfoSection = document.querySelector(".weather-info");
 // weather info elements
 searchButton.addEventListener("click", () => {
   if (cityInput.value.trim() !== "") {
@@ -21,12 +24,20 @@ cityInput.addEventListener("keydown", (e) => {
 async function getFetchData(endPoint, city) {
   // we can replace forecast instead of weather for 5 day forecast
   // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
   const response = await fetch(apiUrl);
   return response.json();
 }
 
 async function updateWeatherInfo(city) {
-  const weatherData = await getFetchData("forecast", city);
+  const weatherData = await getFetchData("weather", city);
+//   not found when data is missed
+if(weatherData.weather !== 200){
+    showDisplaySection(notFoundSection);
+    return;
+}
   console.log(weatherData);
+}
+function showDisplaySection(section) {
+
 }
