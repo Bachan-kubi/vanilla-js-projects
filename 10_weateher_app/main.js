89d7deb51c3ce3f9b70371ca8a4c10f00a8970d1
Bachan Kubi <felixghagra@gmail.com>
2025-10-07 20:39:30 +0600

    logic are being built

--- 10_weateher_app/main.js
+++ 10_weateher_app/main.js
@@ -1,1 +1,18 @@
 const apiKey = "c14f92927d3fbdbfcf9d1934d5bc5655";
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-button");
+// weather info elements
+searchButton.addEventListener("click", () => {
+  if (cityInput.value.trim() !== "") {
+    updateWeatherInfo(cityInput.value)
+    cityInput.value="";
+    cityInput.blur();
+  }
+});
+cityInput.addEventListener("keydown",(e)=>{
+    if(e.key== "Enter" && cityInput.value.trim() !== ""){
+        updateWeatherInfo(cityInput.value);
+        cityInput.value="";
+        cityInput.blur();
+    }
+});
