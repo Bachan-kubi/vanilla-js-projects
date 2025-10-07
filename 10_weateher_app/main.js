a2cf36be20fd193241f93790d0148ab10eb496fe
Bachan Kubi <felixghagra@gmail.com>
2025-10-07 21:04:44 +0600

    logic are being built

--- 10_weateher_app/main.js
+++ 10_weateher_app/main.js
@@ -4,15 +4,29 @@ const searchButton = document.querySelector(".search-button");
 // weather info elements
 searchButton.addEventListener("click", () => {
   if (cityInput.value.trim() !== "") {
-    updateWeatherInfo(cityInput.value)
-    cityInput.value="";
+    updateWeatherInfo(cityInput.value);
+    cityInput.value = "";
     cityInput.blur();
   }
 });
-cityInput.addEventListener("keydown",(e)=>{
-    if(e.key== "Enter" && cityInput.value.trim() !== ""){
-        updateWeatherInfo(cityInput.value);
-        cityInput.value="";
-        cityInput.blur();
-    }
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && cityInput.value.trim() !== "") {
+    updateWeatherInfo(cityInput.value);
+    cityInput.value = "";
+    cityInput.blur();
+  }
 });
+
+// fetching wea
+async function getFetchData(endPoint, city) {
+  // we can replace forecast instead of weather for 5 day forecast
+  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
+  const response = await fetch(apiUrl);
+  return response.json();
+}
+
+async function updateWeatherInfo(city) {
+  const weatherData = await getFetchData("forecast", city);
+  console.log(weatherData);
+}
